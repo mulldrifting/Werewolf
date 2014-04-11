9ac98c2a06a5625599b8e417db1fabbc87e154e4
Lauren Lee <unrealeel@gmail.com>
2014-04-11 12:48:12 -0700

    Initial Commit

---
+++ Werewolf/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Werewolf
+//
+//  Created by Lauren Lee on 4/11/14.
+//  Copyright (c) 2014 Lauren Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
